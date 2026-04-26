265dfc3829aa440041aedb60410f73c362812e87
fulls9186-ux <fulls9186@gmail.com>
2026-04-26 19:35:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +9,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # 讓 Render 自動分配 Port，沒分配時預設用 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
